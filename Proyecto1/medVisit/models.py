4ce9d57d1ccb7eb36d8e3502920ad0a9648704c2
miguelangel0116 <miguel.0116@outlook.com>
2024-10-14 20:56:18 -0500

    subiendo cambios

--- Proyecto1/medVisit/models.py
+++ Proyecto1/medVisit/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+#Este es el nuevo comentario
+
+from django.db import models
+
+# Modelo de Categoría
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo de Producto
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo de Cliente
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
